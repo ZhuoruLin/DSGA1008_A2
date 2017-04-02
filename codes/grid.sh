acfa030ccf6246ed1cd0eb32cab4f831d469de9d
simoninoc <simoninoc@gmail.com>
2017-04-01 20:48:06 -0400

    update

--- codes/grid.sh
+++ codes/grid.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-for emsize in $(50,100);do python main.py --cuda --epochs 1 --emsize $emsize;
+for emsize in {50..550..100}
+	do
+		for nhid in {100..1000..100}
+			do
+				$save = './saved_models/emsiz' 
+				echo "Embedding Size: $emsize Hidden Unit: $nhid"
+				python --cuda --epochs 20 --emsize $emsize --nhid $nhid
+
 done
